5f65cb01c67ebf36037fe9d65f664d11a31941e8
Renata Srbljanin <renatasrbljanin@gmail.com>
2018-08-12 12:10:08 +0200

    first commit

---
+++ src/app/shared/beer/beer.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class BeerService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+//    return this.http.get('http://localhost:8080/good-beers');
+    return this.http.get('https://pwa-server-anxious-klipspringer.cfapps.io/good-beers');
+  }
+
+}
